0a1bfc4195e077fd28b344dea01ce891ca5b17d2
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2026-04-05 19:33:21 +0100

    update

--- Request/Request.hpp
+++ Request/Request.hpp
@@ -29,11 +29,4 @@ class	Request
 
 };
 
-void	parse_reques( std::string buffer, Request &request );
-
-void	get_method( std::string buffer, Request &request );
-void	get_path( std::string buffer, Request &request );
-void	get_version( std::string buffer, Request &request );
-
-
 # endif
